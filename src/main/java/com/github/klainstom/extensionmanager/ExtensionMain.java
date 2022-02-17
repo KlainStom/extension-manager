6a929bcf9ea4e73ad4a2fa40d0c42b244c1c3bc9
offbyone <offbyone@disroot.org>
2022-02-17 14:33:34 +0100

    Add variable holding data directory path

--- src/main/java/com/github/klainstom/extensionmanager/ExtensionMain.java
+++ src/main/java/com/github/klainstom/extensionmanager/ExtensionMain.java
@@ -4,7 +4,29 @@ import com.github.klainstom.extensionmanager.commands.Commands;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.extensions.Extension;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+
 public class ExtensionMain extends Extension {
+    public static Path DATA_DIRECTORY;
+
+    @Override
+    public void preInitialize() {
+        DATA_DIRECTORY = Objects.requireNonNull(MinecraftServer.getExtensionManager()
+                .getExtension("&Name"), "Extension installed but not found!").getDataDirectory();
+
+        if (!DATA_DIRECTORY.toFile().exists()) {
+            try {
+                Files.createDirectory(DATA_DIRECTORY);
+            } catch (IOException e) {
+                MinecraftServer.LOGGER.error("Could not create &Name data directory", e);
+                e.printStackTrace();
+            }
+        }
+    }
+
     @Override
     public void initialize() {
         var commandManager = MinecraftServer.getCommandManager();
